2c9bb0418f56614b757ec2d6bc237bf52c4d6d19
Viktor <ukrvictor1987@gmail.com>
2021-03-31 15:46:15 +0300

    #14

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,22 @@
 "use strict";
 
-if (4 == 9) {
-    console.log("Ok!");
-} else {
-    console.log("Error");
-}
-const num = 50;
+let num = 50;
 
-if (num <49) {
-    console.log("Error");
-} else if (num > 100) {
-    console.log("Very big");
-} else {
-    console.log("OK!");
+// while (num<55){
+//     console.log (num);
+//     num++;
+// }
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num < 55);
+
+for ( let i=1; i<10; i++) {
+    if (i===6) {
+        // break;
+        continue;
+    }
+    console.log(i);
+
 }
