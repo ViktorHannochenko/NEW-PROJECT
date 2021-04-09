16543285c1f4d01365728ad02502749be06e9c8d
Viktor <ukrvictor1987@gmail.com>
2021-04-09 20:08:27 +0300

    #23

--- JS/script.js
+++ JS/script.js
@@ -1,48 +1,20 @@
 "use strict";
-// let a = 5,
-//     b = a;
-//     b = b + 5;
-// console.log(b);
-// console.log(a);
 
-// const obj = {
-//     a: 5,
-//     b: 1
+// console.dir([1, 2, 3]);
+
+const soldier = {
+    health: 400,
+    armor: 200
+};
+
+const jonh = Object.create(soldier);
+
+// const jonh ={
+//     health: 100
 // };
-// const copy = obj;
 
-// copy.a = 10;
-// console.log(copy);
-// console.log(obj);
-function copy(mainObj) {
-    let objCopy = {};
-    let key;
-    for(key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
+// jonh._proto_= soldier;
 
-const numbers = {
-    a:2,
-    b:5,
-    c: {
-        x:7,
-        y:4
-    }
-};
-const newNumbers = copy(numbers);
-newNumbers.a = 10;
-console.log(newNumbers);
-console.log(numbers);
+Object.setPrototypeOf(jonh, soldier);
 
-const add = {
-    d: 17,
-    e: 20
-};
-console.log( Object.assign({}, add));
-
-const video = ['youtube', 'vimeo', 'rutube'],
-    blogs = ['wordpress', 'livejournal','blogger'],
-    internet = [...video, ...blogs, 'vk', 'facebook'];
-    console.log(internet);
+console.log(jonh.armor);
